bb7bd106f70592031b37d5e23a351722902f9d98
Ar-Ray-code <ray255ar@gmail.com>
2022-08-02 20:54:17 +0900

    update header

--- src/micro_ros_arduino_simpler/simpler_base.h
+++ src/micro_ros_arduino_simpler/simpler_base.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "base/unique_executor.hpp"
-#include "base/ip_utils.hpp"
-#include "uros_function/common_uros_function.hpp"
+#include "../base/unique_executor.hpp"
+#include "../base/ip_utils.hpp"
+#include "../uros_function/common_uros_function.hpp"
 
-#include "platform/initialize_stm32eth.hpp"
-#include "platform/initialize_esp32wifi.hpp"
-#include "platform/initialize_usb.hpp"
+#include "../platform/initialize_stm32eth.hpp"
+#include "../platform/initialize_esp32wifi.hpp"
+#include "../platform/initialize_usb.hpp"
 
 
 extern rclc_executor_t executor;
